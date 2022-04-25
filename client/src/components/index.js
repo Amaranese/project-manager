abc126b479138244d57105c3cc99184f2c4d14e2
Amaranese <alejandromaranes@gmail.com>
2022-04-25 12:42:39 +0200

    Create index.js

---
+++ client/src/components/index.js
@@ -0,0 +1,36 @@
+export { default as Board } from "./Board";
+export { default as Task } from "./Task";
+export { default as TaskColumn } from "./TaskColumn";
+export { default as List } from "./List";
+export { default as ListColumn } from "./ListColumn";
+export { default as DndCanvas } from "./DnDCanvas";
+
+export { default as AddBoardModal } from "./AddBoardModal";
+export { default as AddListModal } from "./AddListModal"; // should be named menu
+export { default as AddTaskModal } from "./AddTaskModal"; // should be named menu
+export { default as EditTaskModal } from "./EditTaskModal";
+
+export { default as AssignMemberMenu } from "./AssignMemberMenu";
+export { default as InviteUserMenu } from "./InviteUserMenu";
+export { default as LabelsMenu } from "./LabelsMenu";
+export { default as VisibilityMenu } from "./VisibilityMenu";
+export { default as CoverMenu } from "./CoverMenu";
+export { default as ListMenu } from "./ListMenu";
+export { default as RenameMenu } from "./RenameMenu";
+export { default as BoardDrawer } from "./BoardDrawer";
+
+export { default as Attachment } from "./Attachment";
+export { default as Comment } from "./Comment";
+export { default as Label } from "./Label";
+export { default as SectionTitle } from "./SectionTitle";
+export { default as UserAvatar } from "./UserAvatar";
+
+export { default as Loading } from "./Loading";
+export { default as Alert } from "./Alert";
+
+export { default as GrayButton } from "./GrayButton";
+export { default as LightButton } from "./LightButton";
+
+export { default as AuthForm } from "./AuthForm";
+export { default as EditInput } from "./EditInput";
+export { default as CommentInput } from "./CommentInput";
